b211e59a2b76a769b5ff30277cfae7bb5c6ba5f3
trixenora-s <trixenora@gmail.com>
2026-05-02 18:48:58 +0530

    adeed and updted store provider to use zustand instead of react context api

--- frontend/lib/store-provider.tsx
+++ frontend/lib/store-provider.tsx
@@ -1,8 +1,5 @@
 'use client';
 
-import { useStore } from './store';
-import { Provider } from 'react-redux'; // Not needed for Zustand
-
 // Zustand doesn't need Provider - it's already global
 export function StoreProvider({ children }: { children: React.ReactNode }) {
   return <>{children}</>;
